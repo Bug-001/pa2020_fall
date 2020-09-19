335a48b262977a347d31b1ba14e56150b9ff2e9f
tracer-pa-public <tracer@njuics.org>
2020-09-19 22:04:18 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:04:18 up 12 days, 13:18,  1 user,  load average: 2.96, 2.58, 2.45 > unitime 1600524257830 > cb4b604adb781c771ce2cbd7e1094e9aece088b3

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -52,12 +52,12 @@ uint32_t adder(uint32_t X, uint32_t Y, bool sub, bool useCF, size_t data_size){
     uint32_t lastC = C;
     X = sign_ext(X & (0xFFFFFFFF >> (32 - data_size)), data_size);
     Y = sign_ext(Y & (0xFFFFFFFF >> (32 - data_size)), data_size);
-    printf("Hi\n");
+    if(sub) printf("Hi\n");
     if(sub){
         printb(X); printf(" ");
         printb(Y); printf("\n");
     } 
-    printf("Hi\n");
+    if(sub) printf("Hi\n");
     Y = sub ? ~Y : Y;
     uint32_t result = 0;
     for(int i = data_size; i > 0; --i){
