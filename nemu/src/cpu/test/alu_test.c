4315f6a603fa5640cf36bf7072543e00a1994612
tracer-pa-public <tracer@njuics.org>
2020-09-20 11:00:30 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:00:30 up 13 days,  2:14,  1 user,  load average: 2.16, 2.30, 2.40 > unitime 1600570830361 > 1de8a14cc47b9c59fe0e611a458b9dc95023d86c

--- nemu/src/cpu/test/alu_test.c
+++ nemu/src/cpu/test/alu_test.c
@@ -64,20 +64,6 @@ typedef union {
 	assert(cpu.eflags.SF == test_eflags.SF); \
 	assert(cpu.eflags.ZF == test_eflags.ZF); \
 
-#define assert_res_CPSZ_for_shift(dataSize) \
-		"pushf;" \
-		"popl %%edx;" \
-		: "=a" (res_asm), "=d" (res_eflags) \
-		: "a" (a), "c" (b)); \
-	test_eflags.val = res_eflags; \
-	res_asm = res_asm & (0xFFFFFFFF >> (32 - dataSize)); \
-	fflush(stdout); \
-	assert(res == res_asm); \
-	assert(cpu.eflags.CF == test_eflags.CF); \
-	assert(cpu.eflags.PF == test_eflags.PF); \
-	assert(cpu.eflags.SF == test_eflags.SF); \
-	assert(cpu.eflags.ZF == test_eflags.ZF); \
-
 #define internel_alu_test_CPSZO(alu_func, dataSize, asm_instr) \
 	uint32_t res, res_asm, res_eflags;\
 	TEST_EFLAGS test_eflags;\
@@ -109,13 +95,6 @@ typedef union {
 	res = alu_func(b, a, dataSize);\
 	asm (	asm_instr \
 	assert_res_CPSZ(dataSize)
-	
-#define internel_alu_test_CPSZ_for_shift(alu_func, dataSize, asm_instr) \
-	uint32_t res, res_asm, res_eflags;\
-	TEST_EFLAGS test_eflags;\
-	res = alu_func(b, a, dataSize);\
-	asm (	asm_instr \
-	assert_res_CPSZ_for_shift(dataSize)
 
 void alu_test_add() {
 	uint32_t a, b;
@@ -363,9 +342,9 @@ void alu_test_shl() {
 			a = inputa[i];
 			b = inputb[j];
 			// Modified
-			{internel_alu_test_CPSZ_for_shift(alu_shl,  8, "shlb %%cl, %%al;")}
-			{internel_alu_test_CPSZ_for_shift(alu_shl, 16, "shlw %%cl, %%ax;")}
-			{internel_alu_test_CPSZ_for_shift(alu_shl, 32, "shll %%cl, %%eax;")}
+			{internel_alu_test_CPSZ(alu_shl,  8, "shlb %%cl, %%al;")}
+			{internel_alu_test_CPSZ(alu_shl, 16, "shlw %%cl, %%ax;")}
+			{internel_alu_test_CPSZ(alu_shl, 32, "shll %%cl, %%eax;")}
 		}
 	}
 
