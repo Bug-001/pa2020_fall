ece6196bfe11b6b3c226aa1902820469a78c3d95
tracer-pa-public <tracer@njuics.org>
2020-09-20 11:39:54 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:39:54 up 13 days,  2:54,  1 user,  load average: 2.35, 2.45, 2.45 > unitime 1600573194294 > 8736303ffcd5773e245c5a503ae27bba16fda3f1

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -172,7 +172,7 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size)
 #else
     src &= (0xFFFFFFFF >> (32 - data_size));
     dest &= (0xFFFFFFFF >> (32 - data_size));
-	uint64_t res = (uint64_t)src * (uint64_t)dest;
+	uint64_t res = src * dest;
 	cpu.eflags.CF = res >> data_size ? 1 : 0;
 	cpu.eflags.OF = res >> data_size ? 1 : 0;
 	return res;
@@ -184,10 +184,10 @@ int64_t alu_imul(int32_t src, int32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_imul(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)));
+	dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)));
+	int64_t res = src * dest;
+	return res;
 #endif
 }
 
