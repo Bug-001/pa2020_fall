4a920077cbcf26be88b77a9cdf5ece1ee4b0b452
tracer-pa-public <tracer@njuics.org>
2020-09-20 09:34:42 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 09:34:42 up 13 days, 49 min,  1 user,  load average: 2.39, 2.41, 2.40 > unitime 1600565682412 > 7ba32e7db7cc55a2639e2eb46f6b9c33b657ef2b

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -270,7 +270,7 @@ uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size)
 #else
     printb(src, data_size); printf(" ");
     printb(dest, data_size); printf("\n");
-    printb(dest << src, data_size); printf("\n");
+    printb(src << dest, data_size); printf("\n");
     return src << dest;
 	// return shift(src, dest, 2, data_size);
 #endif
