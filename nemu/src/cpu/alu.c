601ba8d6eac97516ff191427b1b0c017e033670c
tracer-pa-public <tracer@njuics.org>
2020-09-20 12:53:15 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 12:53:15 up 13 days,  4:07,  1 user,  load average: 2.48, 2.40, 2.37 > unitime 1600577595706 > 3c4eb0449e8f87f9f929da822cc4431446baa655

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -175,8 +175,8 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size)
 	uint64_t res = src64 * dest64;
 	cpu.eflags.CF = res >> data_size ? 1 : 0;
 	cpu.eflags.OF = res >> data_size ? 1 : 0;
-	// return res;
-	return res & (0xFFFFFFFFFFFFFFFF >> (64 - 2 * data_size));
+	return res;
+	// return res & (0xFFFFFFFFFFFFFFFF >> (64 - 2 * data_size));
 #endif
 }
 
@@ -212,10 +212,10 @@ int32_t alu_idiv(int64_t src, int64_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_idiv(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+    src &= (0xFFFFFFFF >> (32 - data_size));
+    dest &= (0xFFFFFFFF >> (32 - data_size));
+    int32_t res = dest / src;
+    return res;
 #endif
 }
 
