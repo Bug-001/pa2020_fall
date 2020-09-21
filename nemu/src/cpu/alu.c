78e697e679c7a1f90408ad3eef53087df8e9fb12
tracer-pa-public <tracer@njuics.org>
2020-09-21 21:19:24 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:19:24 up 14 days, 12:33,  1 user,  load average: 2.83, 2.66, 2.50 > unitime 1600694363966 > b34fdb3f23725b76913e1cbee302d6f88567c4f

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -192,6 +192,7 @@ int64_t alu_imul(int32_t src, int32_t dest, size_t data_size)
 	int64_t res = src64 * dest64;
 	count++;
 	if(count > 10) exit(-1);
+	printf("src=%d, dest=%d\n", src, dest);
 	printb(res,64); printf("\n");
 	printb(res & (0xFFFFFFFFFFFFFFFF >> (64 - 2 * data_size)),64); printf("\n\n");
 	// return res;
