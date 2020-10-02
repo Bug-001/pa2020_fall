8f36b4cfd9eb8d886d1b30f4320c034e0d39a68d
tracer-pa-public <tracer@njuics.org>
2020-10-02 17:16:58 +0800

    > clean > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:16:58 up 25 days,  8:31,  2 users,  load average: 3.97, 3.49, 3.35 > unitime > f4199c7dd753343d8afa4c06aacef13b7f16bafa

--- nemu/src/monitor/ui.c
+++ nemu/src/monitor/ui.c
@@ -68,11 +68,7 @@ cmd_handler(cmd_si)
 
 cmd_handler(cmd_info)
 {
-    if(args == NULL)
-	{
-	    printf("args needed (\"w\" or \"r\")\n");
-	    return 0;
-	}
+    /* TODO: repair the BUG */
 	char *arg = strtok(NULL, " ");
 	if (strcmp(arg, "r") == 0)
 	{
