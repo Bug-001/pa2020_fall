d5176299ed1999b591b62b0d2330a31de23b946a
tracer-pa-public <tracer@njuics.org>
2020-10-05 00:13:33 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 00:13:33 up 27 days, 15:28,  2 users,  load average: 4.08, 3.65, 3.49 > unitime 1601828013501 > 8e544702e042207119c766e8817284e0a977768e

--- nemu/src/cpu/instr/inv.c
+++ nemu/src/cpu/instr/inv.c
@@ -52,7 +52,7 @@ static void add_inv_log(uint32_t eip, uint8_t *p)
 	struct tm *tmp_ptr = NULL;
 	time(&tmpcal_ptr);
 	tmp_ptr = localtime(&tmpcal_ptr);
-    fprintf(fp, " %d:%d:%d %d:%d:%d  invalid opcode(eip = 0x%08x): %02x %02x %02x %02x %02x %02x %02x %02x ...\n",
+    fprintf(fp, " %u:%u:%u %u:%u:%u  invalid opcode(eip = 0x%08x): %02x %02x %02x %02x %02x %02x %02x %02x ...\n",
          (1900+tmp_ptr->tm_year), (1+tmp_ptr->tm_mon), tmp_ptr->tm_mday), tmp_ptr->tm_hour, tmp_ptr->tm_min, tmp_ptr->tm_sec, 
          eip, p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7]);
     fprintf(fp, "%d", count + 1);
