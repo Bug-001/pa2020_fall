9c425a607cbae0ef610ec9ac966c7517c770315e
tracer-pa-public <tracer@njuics.org>
2020-10-04 20:33:18 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:33:18 up 27 days, 11:47,  2 users,  load average: 3.18, 3.08, 3.22 > unitime 1601814797955 > e3b593aa809a640035c640441bdfff47d9e5ccc5

--- nemu/include/cpu/instr/cmp.h
+++ nemu/include/cpu/instr/cmp.h
@@ -1,7 +1,26 @@
 #ifndef __INSTR_CMP_H__
 #define __INSTR_CMP_H__
-/*
-Put the declarations of `cmp' instructions here.
-*/
+
+make_instr_func(cmp_r2rm_b); // 0x38
+make_instr_func(mov_r2rm_v);
+make_instr_func(mov_rm2r_b);
+make_instr_func(mov_rm2r_v);
+make_instr_func(mov_i2rm_b);
+make_instr_func(mov_i2rm_v);
+make_instr_func(mov_i2r_b);
+make_instr_func(mov_i2r_v);
+make_instr_func(mov_o2a_b);
+make_instr_func(mov_o2a_v);
+make_instr_func(mov_a2o_b);
+make_instr_func(mov_a2o_v);
+make_instr_func(mov_zrm82r_v);
+make_instr_func(mov_zrm162r_l);
+make_instr_func(mov_srm82r_v);
+make_instr_func(mov_srm162r_l);
+
+make_instr_func(cmp_r2rm_b); // 0x38
+make_instr_func(mov_r2rm_v); // 0x39
+make_instr_func(cmp_rm2r_b); // 0x3A
+make_instr_func(cmp_rm2r_v); // 0x3B
 
 #endif
