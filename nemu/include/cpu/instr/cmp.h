25a06dcf6f22635f48a98aa291871b95b4719b2e
tracer-pa-public <tracer@njuics.org>
2020-10-04 21:10:09 +0800

    > clean > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:10:09 up 27 days, 12:24,  2 users,  load average: 4.76, 3.68, 3.41 > unitime > 2ca86501c713c57c7ff1d23d1f12421e2fdf5e4a

--- nemu/include/cpu/instr/cmp.h
+++ nemu/include/cpu/instr/cmp.h
@@ -1,26 +1,14 @@
 #ifndef __INSTR_CMP_H__
 #define __INSTR_CMP_H__
 
-make_instr_func(cmp_r2rm_b); // 0x38
-make_instr_func(mov_r2rm_v);
-make_instr_func(mov_rm2r_b);
-make_instr_func(mov_rm2r_v);
-make_instr_func(mov_i2rm_b);
-make_instr_func(mov_i2rm_v);
-make_instr_func(mov_i2r_b);
-make_instr_func(mov_i2r_v);
-make_instr_func(mov_o2a_b);
-make_instr_func(mov_o2a_v);
-make_instr_func(mov_a2o_b);
-make_instr_func(mov_a2o_v);
-make_instr_func(mov_zrm82r_v);
-make_instr_func(mov_zrm162r_l);
-make_instr_func(mov_srm82r_v);
-make_instr_func(mov_srm162r_l);
+make_instr_func(cmp_r2rm_b);    // 0x38
+make_instr_func(mov_r2rm_v);    // 0x39
+make_instr_func(cmp_rm2r_b);    // 0x3A
+make_instr_func(cmp_rm2r_v);    // 0x3B
+make_instr_func(cmp_i2a_b);     // 0x3C
+make_instr_func(cmp_i2a_v);     // 0x3D
+make_instr_func(cmp_i2rm_b);    // 0x80
+make_instr_func(cmp_i2rm_v);    // 0x81, 0x83
 
-make_instr_func(cmp_r2rm_b); // 0x38
-make_instr_func(mov_r2rm_v); // 0x39
-make_instr_func(cmp_rm2r_b); // 0x3A
-make_instr_func(cmp_rm2r_v); // 0x3B
 
 #endif
--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -1,4 +1,28 @@
 #include "cpu/instr.h"
-/*
-Put the implementations of `cmp' instructions here.
-*/
+// #include "cpu/alu.h"
+
+static void instr_execute_2op()
+{
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+    uint32_t opr_src_temp = sign_ext(opr_src.val, opr_dest.data_size);
+    uint32_t _ = alu_sub(opr_src_temp, opr_dest.val, opr_dest.data_size);
+}
+
+// make_instr_func(cmp_r2rm_b);    // 0x38
+// make_instr_func(mov_r2rm_v);    // 0x39
+// make_instr_func(cmp_rm2r_b);    // 0x3A
+// make_instr_func(cmp_rm2r_v);    // 0x3B
+// make_instr_func(cmp_i2a_b);     // 0x3C
+// make_instr_func(cmp_i2a_v);     // 0x3D
+// make_instr_func(cmp_i2rm_b);    // 0x80
+// make_instr_func(cmp_i2rm_v);    // 0x81, 0x83
+
+make_instr_impl_2op(cmp, r, rm, b)
+make_instr_impl_2op(cmp, r, rm, v)
+make_instr_impl_2op(cmp, rm, r, b)
+make_instr_impl_2op(cmp, rm, r, v)
+make_instr_impl_2op(cmp, i, a, b)
+make_instr_impl_2op(cmp, i, a, v)
+make_instr_impl_2op(cmp, i, rm, b)
+make_instr_impl_2op(cmp, i, rm, v)
