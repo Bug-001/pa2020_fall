e5714af9ed38fc0ece28bf22b58ae475d9294ccb
tracer-pa-public <tracer@njuics.org>
2020-10-06 00:17:36 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 00:17:36 up 28 days, 15:32,  3 users,  load average: 3.18, 3.33, 3.46 > unitime 1601914656075 > f275c0ec7198dfcf668fd33aecb9917a68df9b68

--- nemu/include/cpu/instr/push.h
+++ nemu/include/cpu/instr/push.h
@@ -3,5 +3,6 @@
 
 make_instr_func(push_r_v);
 make_instr_func(push_rm_v);
+make_instr_func(push_i_b);
 
 #endif
--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -18,3 +18,4 @@ static void instr_execute_1op()
 
 make_instr_impl_1op(push, r, v)
 make_instr_impl_1op(push, rm, v)
+make_instr_impl_1op(push, i, b)
