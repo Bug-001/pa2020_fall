ac6c148ac48361c319f030488fe88ba875ab934e
tracer-pa-public <tracer@njuics.org>
2020-10-05 14:05:59 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 14:05:59 up 28 days,  5:20,  2 users,  load average: 3.17, 3.42, 3.47 > unitime 1601877959469 > a1dcf5983ddc1d61ab8db3831bca09046b1041e

--- nemu/include/cpu/instr/fundamental_cal.h
+++ nemu/include/cpu/instr/fundamental_cal.h
@@ -1,23 +1,23 @@
 #include "cpu/instr.h"
 
-#define fc_make_instr_impls(name)               
-    make_instr_impl_2op(name, r, rm, b)         \
-    make_instr_impl_2op(name, r, rm, v)         \
-    make_instr_impl_2op(name, rm, r, b)         \
-    make_instr_impl_2op(name, rm, r, v)         \
-    make_instr_impl_2op(name, i, a, b)          \
-    make_instr_impl_2op(name, i, a, v)          \
-    make_instr_impl_2op(name, i, rm, b)         \
-    make_instr_impl_2op(name, i, rm, v)         \
-    make_instr_impl_2op(name, i, rm, bv)
+#define fc_make_instr_impls(inst_name)               \
+    make_instr_impl_2op(inst_name, r, rm, b)         \
+    make_instr_impl_2op(inst_name, r, rm, v)         \
+    make_instr_impl_2op(inst_name, rm, r, b)         \
+    make_instr_impl_2op(inst_name, rm, r, v)         \
+    make_instr_impl_2op(inst_name, i, a, b)          \
+    make_instr_impl_2op(inst_name, i, a, v)          \
+    make_instr_impl_2op(inst_name, i, rm, b)         \
+    make_instr_impl_2op(inst_name, i, rm, v)         \
+    make_instr_impl_2op(inst_inst_name, i, rm, bv)
     
-#define fc_make_instr_funcs(name)               
-    make_instr_func(concat(name, _r2rm_b));     \
-    make_instr_func(concat(name, _r2rm_v));     \
-    make_instr_func(concat(name, _rm2r_b));     \
-    make_instr_func(concat(name, _rm2r_v));     \
-    make_instr_func(concat(name, _i2a_b));      \
-    make_instr_func(concat(name, _i2a_v));      \
-    make_instr_func(concat(name, _i2rm_b));     \
-    make_instr_func(concat(name, _i2rm_v));     \
-    make_instr_func(concat(name, _i2rm_bv));
+#define fc_make_instr_funcs(inst_name)               \
+    make_instr_func(concat(inst_name, _r2rm_b));     \
+    make_instr_func(concat(inst_name, _r2rm_v));     \
+    make_instr_func(concat(inst_name, _rm2r_b));     \
+    make_instr_func(concat(inst_name, _rm2r_v));     \
+    make_instr_func(concat(inst_name, _i2a_b));      \
+    make_instr_func(concat(inst_name, _i2a_v));      \
+    make_instr_func(concat(inst_name, _i2rm_b));     \
+    make_instr_func(concat(inst_name, _i2rm_v));     \
+    make_instr_func(concat(inst_name, _i2rm_bv));
