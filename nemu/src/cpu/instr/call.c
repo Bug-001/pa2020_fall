5046e1e8eff03d88916c8db49169bb158bda7a88
tracer-pa-public <tracer@njuics.org>
2020-10-05 18:29:28 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:29:28 up 28 days,  9:43,  3 users,  load average: 3.40, 3.60, 3.54 > unitime 1601893768010 > 1a1aea12abcdd92e9f5ecb3a7680fd2d5d0c0404

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -10,7 +10,8 @@ static int call_near_(bool indirect)
     
     opr_dest.type = OPR_MEM;
     opr_dest.addr = cpu.esp;
-    opr_dest.val = cpu.eip + 1 + data_size / 8;
+    // opr_dest.val = cpu.eip + 1 + data_size / 8;
+    opr_dest.val = eip;
     operand_write(&opr_dest);
     
     if(!indirect)
