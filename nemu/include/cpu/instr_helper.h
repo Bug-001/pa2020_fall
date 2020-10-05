ccd76e87f2c6b9d5d7d1de94912e88df3b670722
tracer-pa-public <tracer@njuics.org>
2020-10-05 17:50:55 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:50:55 up 28 days,  9:05,  2 users,  load average: 3.84, 3.55, 3.45 > unitime 1601891455328 > d57f12e22048ddb42418388ea864bd4f92a7993

--- nemu/include/cpu/instr_helper.h
+++ nemu/include/cpu/instr_helper.h
@@ -196,7 +196,7 @@ void print_asm_3(char *instr, char *suffix, uint8_t len, OPERAND *opr_1, OPERAND
 	cpu.eflags.ZF == 0
 
 #define condition_na \
-	cpu.eflags.CF == 1 && cpu.eflags.ZF == 1 // Modified
+	cpu.eflags.CF == 1 || cpu.eflags.ZF == 1
 
 #define condition_no \
 	cpu.eflags.OF == 0
