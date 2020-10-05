f21c4e7ed97431fe6d2febdff0c39d7b5833356a
tracer-pa-public <tracer@njuics.org>
2020-10-05 11:55:11 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:55:11 up 28 days,  3:09,  2 users,  load average: 3.36, 3.46, 3.54 > unitime 1601870111692 > 22c81f3f88b0c5869d1042f14f540f186dd12e5

--- nemu/include/cpu/instr/test.h
+++ nemu/include/cpu/instr/test.h
@@ -1,7 +1,13 @@
 #ifndef __INSTR_TEST_H__
 #define __INSTR_TEST_H__
-/*
-Put the declarations of `test' instructions here.
-*/
+
+make_instr_func(test_i2a_b);    // 0xA8
+make_instr_func(test_i2a_v);    // 0xA9
+
+make_instr_func(test_i2rm_b);   // 0xF6
+make_instr_func(test_i2rm_v);   // 0xF7
+
+make_instr_func(test_r2rm_b);   // 0x84
+make_instr_func(test_r2rm_v);   // 0x85
 
 #endif
--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -1,4 +1,26 @@
 #include "cpu/instr.h"
-/*
-Put the implementations of `test' instructions here.
-*/
+
+static void instr_execute_2op
+{
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+    alu_and(opr_src.val, opr_dest.val);
+}
+
+// make_instr_func(test_i2a_b);    // 0xA8
+// make_instr_func(test_i2a_v);    // 0xA9
+
+// make_instr_func(test_i2rm_b);   // 0xF6
+// make_instr_func(test_i2rm_v);   // 0xF7
+
+// make_instr_func(test_r2rm_b);   // 0x84
+// make_instr_func(test_r2rm_v);   // 0x85
+
+make_instr_impl_2op(test, i, a, b)
+make_instr_impl_2op(test, i, a, v)
+
+make_instr_impl_2op(test, i, rm, b)
+make_instr_impl_2op(test, i, rm, v)
+
+make_instr_impl_2op(test, r, rm, b)
+make_instr_impl_2op(test, r, rm, v)
