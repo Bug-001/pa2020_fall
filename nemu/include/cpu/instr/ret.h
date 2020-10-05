2097d186f678ee0f6e4935e232ef7ca413b384f7
tracer-pa-public <tracer@njuics.org>
2020-10-05 10:31:15 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:31:15 up 28 days,  1:45,  2 users,  load average: 2.94, 3.29, 3.46 > unitime 1601865074948 > e0e647b423603b981414eb32154fbdd139d9a6d

--- nemu/include/cpu/instr/ret.h
+++ nemu/include/cpu/instr/ret.h
@@ -1,7 +1,7 @@
 #ifndef __INSTR_RET_H__
 #define __INSTR_RET_H__
-/*
-Put the declarations of `ret' instructions here.
-*/
+
+make_instr_func(ret_near);
+make_instr_func(ret_near_imm16);
 
 #endif
--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -1,4 +1,35 @@
 #include "cpu/instr.h"
-/*
-Put the implementations of `ret' instructions here.
-*/
+
+static int ret_near_(bool imm16)
+{
+    opr_src.type = OPR_MEM;
+    opr_src.sreg = SREG_CS;
+    opr_src.addr = cpu.esp;
+    
+    operand_read(&opr_src);
+    cpu.esp += data_size / 8;
+    
+    if(imm16)
+    {
+        opr_dest.type = OPR_IMM;
+        opr_dest.sreg = SREG_CS;
+        opr_dest.addr = cpu.eip + 1;
+        opr_dest.data_size = 16;
+        operand_read(&opr_dest);
+        cpu.esp += opr_dest.val;
+    }
+    
+    cpu.eip = opr_src.val & (0xFFFFFFFF >> (32 - data_size));
+    
+    return 0;
+}
+
+make_instr_func(ret_near)
+{
+    return ret_near(0);
+}
+
+make_instr_func(ret_near_imm16)
+{
+    return ret_near_(1);
+}
