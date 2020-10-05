db7ac29b80d0f54f042b76c5b47d8e679a140300
tracer-pa-public <tracer@njuics.org>
2020-10-05 14:07:00 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 14:07:00 up 28 days,  5:21,  2 users,  load average: 3.57, 3.47, 3.49 > unitime 1601878019839 > 622bf487824e607e58f7ba7bda9cbf75f96295e7

--- nemu/include/cpu/instr/fundamental_cal.h
+++ nemu/include/cpu/instr/fundamental_cal.h
@@ -1,6 +1,6 @@
 #include "cpu/instr.h"
 
-#define fc_make_instr_impls(inst_name)               \
+#define fc_make_instr_impls(inst_name) 
     make_instr_impl_2op(inst_name, r, rm, b)         \
     make_instr_impl_2op(inst_name, r, rm, v)         \
     make_instr_impl_2op(inst_name, rm, r, b)         \
@@ -11,7 +11,7 @@
     make_instr_impl_2op(inst_name, i, rm, v)         \
     make_instr_impl_2op(inst_inst_name, i, rm, bv)
     
-#define fc_make_instr_funcs(inst_name)               \
+#define fc_make_instr_funcs(inst_name) 
     make_instr_func(concat(inst_name, _r2rm_b));     \
     make_instr_func(concat(inst_name, _r2rm_v));     \
     make_instr_func(concat(inst_name, _rm2r_b));     \
