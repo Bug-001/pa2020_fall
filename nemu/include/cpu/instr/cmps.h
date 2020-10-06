e4457f766711625ac09fc4cddfb14613825aff52
tracer-pa-public <tracer@njuics.org>
2020-10-06 11:15:22 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:15:22 up 29 days,  2:29,  3 users,  load average: 3.34, 3.25, 3.34 > unitime 1601954122227 > ebb1fd33d321dac1e50c98f099441aabc4a42ab8

--- nemu/include/cpu/instr/cmps.h
+++ nemu/include/cpu/instr/cmps.h
@@ -1,7 +1,7 @@
 #ifndef __INSTR_CMPS_H__
 #define __INSTR_CMPS_H__
 
-make_instr_func(cmps_v);
-make_instr_func(cmps_b);
+make_instr_func(cmps_v);    // 0xA7
+make_instr_func(cmps_b);    // 0xA6
 
 #endif
