c3ec10fdfebdf35c517f5159156f60b8be9b425a
tracer-pa-public <tracer@njuics.org>
2020-10-06 10:48:20 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:48:20 up 29 days,  2:02,  3 users,  load average: 3.45, 3.52, 3.58 > unitime 1601952500394 > f894500d26c7361187f698c42804ecebe9a6f6f1

--- nemu/include/cpu/instr/setcc.h
+++ nemu/include/cpu/instr/setcc.h
@@ -1,21 +1,22 @@
 #ifndef __INSTR_SETCC_H__
 #define __INSTR_SETCC_H__
 
-make_instr_func(setne_b);
-make_instr_func(seta_b);
-make_instr_func(setae_b);
-make_instr_func(setbe_b);
-make_instr_func(setc_b);
-make_instr_func(sete_b);
-make_instr_func(setg_b);
-make_instr_func(setge_b);
-make_instr_func(setl_b);
-make_instr_func(setle_b);
-make_instr_func(setno_b);
-make_instr_func(setnp_b);
-make_instr_func(setns_b);
-make_instr_func(seto_b);
-make_instr_func(setp_b);
-make_instr_func(sets_b);
+// 0x0F
+make_instr_func(setne_b);   // 0x95
+make_instr_func(seta_b);    // 0x97
+make_instr_func(setae_b);   // 0x93
+make_instr_func(setbe_b);   // 0x96
+make_instr_func(setc_b);    // 0x92
+make_instr_func(sete_b);    // 0x94
+make_instr_func(setg_b);    // 0x9F
+make_instr_func(setge_b);   // 0x9D
+make_instr_func(setl_b);    // 0x9C
+make_instr_func(setle_b);   // 0x9E
+make_instr_func(setno_b);   // 0x91
+make_instr_func(setnp_b);   // 0x9B
+make_instr_func(setns_b);   // 0x99
+make_instr_func(seto_b);    // 0x90
+make_instr_func(setp_b);    // 0x9A
+make_instr_func(sets_b);    // 0x98
 
 #endif
