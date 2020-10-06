d7eaa88c1682256560cff428fa056a5993ec759e
tracer-pa-public <tracer@njuics.org>
2020-10-06 10:08:46 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:08:46 up 29 days,  1:23,  3 users,  load average: 4.79, 3.92, 3.56 > unitime 1601950126433 > 48d0fae172130c6160b9ce8e0f5adce1cdad9ed

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -4,7 +4,8 @@ static void instr_execute_2op()
 {
     operand_read(&opr_src);
     operand_read(&opr_dest);
-    opr_dest.val = alu_sub(opr_src.val, opr_dest.val, data_size);
+    opr_src_temp = sign_ext(opr_src.val, opr_dest.data_size);
+    opr_dest.val = alu_sub(opr_src_temp, opr_dest.val, opr_dest.data_size);
     operand_write(&opr_dest);
 }
 
--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -4,6 +4,7 @@ static void instr_execute_2op()
 {
     operand_read(&opr_src);
     operand_read(&opr_dest);
+    assert(opr_src.data_size == opr_dest.data_size);
     alu_and(opr_src.val, opr_dest.val, data_size);
 }
 
