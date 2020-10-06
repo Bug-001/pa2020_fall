61bdd65eeaaa0ded3eceadd559496dad37326492
tracer-pa-public <tracer@njuics.org>
2020-10-06 14:54:32 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 14:54:32 up 29 days,  6:09,  3 users,  load average: 3.10, 3.20, 3.30 > unitime 1601967272173 > fdc56496a98202b0bcca668a021812f1f3c2385

--- nemu/src/cpu/instr/not.c
+++ nemu/src/cpu/instr/not.c
@@ -3,7 +3,7 @@
 static void instr_execute_1op()
 {
     operand_read(&opr_src);
-    opr_src.val = (~opr_src.val) & (0xFFFFFFFF >> (32 - opr_src.datasize));
+    opr_src.val = (~opr_src.val) & (0xFFFFFFFF >> (32 - opr_src.data_size));
     operand_write(&opr_src);
 }
 
