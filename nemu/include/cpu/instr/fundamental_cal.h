f81ac53c393b7893f08382f54d04d3e553a1730f
tracer-pa-public <tracer@njuics.org>
2020-10-06 15:22:19 +0800

    > clean > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:22:19 up 29 days,  6:36,  3 users,  load average: 4.92, 3.93, 3.56 > unitime > 89f49f3071a9496a8bdf58ba51c3e9b964ab99cc

--- nemu/include/cpu/instr/fundamental_cal.h
+++ nemu/include/cpu/instr/fundamental_cal.h
@@ -29,9 +29,13 @@
         operand_read(&opr_src); \
         operand_read(&opr_dest); \
         uint32_t opr_src_temp = sign_ext(opr_src.val, opr_src.data_size); \
-        opr_dest.val = concat(alu_, inst_name)(opr_src_temp, opr_dest.val, opr_dest.data_size); \
         if(strcmp(str(inst_name), "cmp")) \
         { \
+            opr_dest.val = concat(alu_, inst_name)(opr_src_temp, opr_dest.val, opr_dest.data_size); \
             operand_write(&opr_dest); \
         } \
+        else
+        {
+            alu_sub(opr_src_temp, opr_dest.val, opr_dest.data_size); \
+        }
     }
