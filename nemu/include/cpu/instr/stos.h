bcb796c0db23cc37fef0d9f4fa2cc2924e676e33
tracer-pa-public <tracer@njuics.org>
2020-10-06 11:13:11 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:13:11 up 29 days,  2:27,  3 users,  load average: 3.54, 3.38, 3.41 > unitime 1601953991228 > 3fea8cb36d62b31d7ea3f5301e011c6aa5b194d4

--- nemu/include/cpu/instr/stos.h
+++ nemu/include/cpu/instr/stos.h
@@ -1,7 +1,7 @@
 #ifndef __INSTR_STOS_H__
 #define __INSTR_STOS_H__
 
-make_instr_func(stos_b);
-make_instr_func(stos_v);
+make_instr_func(stos_b);    // 0xAA
+make_instr_func(stos_v);    // 0xAB
 
 #endif
