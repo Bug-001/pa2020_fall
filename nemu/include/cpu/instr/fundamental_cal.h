a07941382205d9a21059399cb109602fc338b6a2
tracer-pa-public <tracer@njuics.org>
2020-10-06 16:08:19 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:08:19 up 29 days,  7:22,  3 users,  load average: 3.31, 3.49, 3.44 > unitime 1601971699704 > b82c890e25a4bb779e7da22b7a27348287cd1688

--- nemu/include/cpu/instr/fundamental_cal.h
+++ nemu/include/cpu/instr/fundamental_cal.h
@@ -29,8 +29,7 @@
     { \
         operand_read(&opr_src); \
         operand_read(&opr_dest); \
-        opr_dest.val = concat(alu_, inst_name)(opr_src.val, opr_dest.val, opr_dest.data_size); \
+        uint32_t opr_src_temp = sign_ext(opr_src.val, opr_src.data_size); \
+        opr_dest.val = concat(alu_, inst_name)(opr_src_temp, opr_dest.val, opr_dest.data_size); \
         operand_write(&opr_dest); \
     }
-    
-// uint32_t opr_src_temp = sign_ext(opr_src.val, opr_src.data_size);
