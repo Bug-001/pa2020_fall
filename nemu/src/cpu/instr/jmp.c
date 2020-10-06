b54ced8a008083256e497c5a7c2e7b4cd80b1e22
tracer-pa-public <tracer@njuics.org>
2020-10-06 13:38:35 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 13:38:35 up 29 days,  4:53,  3 users,  load average: 3.36, 3.30, 3.31 > unitime 1601962714848 > 16bc88fe884cce5017980e895abaa188fd37fda

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -48,7 +48,8 @@ make_instr_func(jmp_near_indirect)
     ind.type = OPR_IMM;
     ind.sreg = SREG_CS;
     ind.data_size = 32;
-    ind.addr = eip + 3;
+    ind.addr = eip + 1;
+    int len = modrm_rm(eip + 1, &ind);
     operand_read(&ind);
     printf("ind.val == %x\n", ind.val);
     print_asm_1("jmp", "", 7, &ind);
