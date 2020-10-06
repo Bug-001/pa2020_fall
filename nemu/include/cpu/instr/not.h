b76231a2d8333ca177e3165aaafc064b80469c90
tracer-pa-public <tracer@njuics.org>
2020-10-06 10:54:24 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:54:24 up 29 days,  2:08,  3 users,  load average: 3.89, 3.62, 3.59 > unitime 1601952864179 > 518ad994f9527b1faf1607d324ba3f82abae206a

--- nemu/include/cpu/instr/not.h
+++ nemu/include/cpu/instr/not.h
@@ -1,7 +1,7 @@
 #ifndef __INSTR_NOT_H__
 #define __INSTR_NOT_H__
 
-make_instr_func(not_rm_b)   // 0xF6
-make_instr_func(not_rm_v)   // 0xF7
+make_instr_func(not_rm_b);   // 0xF6
+make_instr_func(not_rm_v);   // 0xF7
 
 #endif
