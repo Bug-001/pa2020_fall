3215ba6e43d368dec5845ce78b07313d58de6327
tracer-pa-public <tracer@njuics.org>
2020-10-06 11:08:23 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 8b55047fac52 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:08:23 up 29 days,  2:22,  3 users,  load average: 3.28, 3.20, 3.37 > unitime 1601953703539 > 93d8914f74e5ac90e783c968a3c971c7a5ce7a2

--- nemu/include/cpu/instr/div.h
+++ nemu/include/cpu/instr/div.h
@@ -1,9 +1,9 @@
 #ifndef __INSTR_DIV_H__
 #define __INSTR_DIV_H__
 
-make_instr_func(idiv_rm2a_b);
-make_instr_func(idiv_rm2a_v);
-make_instr_func(div_rm2a_b);
-make_instr_func(div_rm2a_v);
+make_instr_func(idiv_rm2a_b);   // 0xF6-7
+make_instr_func(idiv_rm2a_v);   // 0xF7-7
+make_instr_func(div_rm2a_b);    // 0xF6-6
+make_instr_func(div_rm2a_v);    // 0xF7-6
 
 #endif
