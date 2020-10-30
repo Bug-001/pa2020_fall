991c2899d161efee5d81dc610f2f17ab671732b7
tracer-pa-public <tracer@njuics.org>
2020-10-30 23:25:14 +0800

    > clean > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:25:14 up 53 days, 14:39,  1 user,  load average: 2.31, 2.33, 2.36 > unitime > e99c0e8e1a9745a47a553c7edcda1086935184

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -19,16 +19,25 @@ void hw_mem_write(paddr_t paddr, size_t len, uint32_t data)
 	memcpy(hw_mem + paddr, &data, len);
 }
 
-uint32_t paddr_read(paddr_t paddr, size_t len)
+uint32_t paddr_read(paddr_t paddr, size_t len) 
 {
 	uint32_t ret = 0;
-	ret = hw_mem_read(paddr, len);
+#ifdef CACHE_ENABLED
+		ret = cache_read(paddr, len);     // 通过cache进行读
+#else
+		ret = hw_mem_read(paddr, len);
+#endif
 	return ret;
 }
 
-void paddr_write(paddr_t paddr, size_t len, uint32_t data)
+
+void paddr_write(paddr_t paddr, size_t len, uint32_t data) 
 {
-	hw_mem_write(paddr, len, data);
+#ifdef CACHE_ENABLED
+		cache_write(paddr, len, data);    // 通过cache进行写
+#else
+		hw_mem_write(paddr, len, data);
+#endif
 }
 
 uint32_t laddr_read(laddr_t laddr, size_t len)
@@ -57,12 +66,15 @@ void init_mem()
 {
 	// clear the memory on initiation
 	memset(hw_mem, 0, MEM_SIZE_B);
-
+#ifdef CACHE_ENABLED
+	init_cache();                             // 初始化cache
+#endif
 #ifdef TLB_ENABLED
 	make_all_tlb();
 	init_all_tlb();
 #endif
 }
+
 
 uint32_t instr_fetch(vaddr_t vaddr, size_t len)
 {
