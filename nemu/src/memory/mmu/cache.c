29d1c354b9b8fed4a612f7410556ebc546ffaa46
tracer-pa-public <tracer@njuics.org>
2020-10-30 23:26:54 +0800

    > clean > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:26:54 up 53 days, 14:41,  1 user,  load average: 2.45, 2.40, 2.38 > unitime > 1439dd3e1ebee7477d604730cbfbb2ae77542554

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -3,18 +3,27 @@
 // init the cache
 void init_cache()
 {
+    printf("cache: implement me in PA 3-1\n");
+    fflush(stdout);
+    assert(0);
 	// implement me in PA 3-1
 }
 
 // write data to cache
 void cache_write(paddr_t paddr, size_t len, uint32_t data)
 {
+    printf("cache: implement me in PA 3-1\n");
+    fflush(stdout);
+    assert(0);
 	// implement me in PA 3-1
 }
 
 // read data from cache
 uint32_t cache_read(paddr_t paddr, size_t len)
 {
+    printf("cache: implement me in PA 3-1\n");
+    fflush(stdout);
+    assert(0);
 	// implement me in PA 3-1
 	return 0;
 }
