632d8c04a6b999f58081a2012e1f825548f01766
tracer-pa-public <tracer@njuics.org>
2020-10-30 23:28:29 +0800

    > clean > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:28:29 up 53 days, 14:42,  1 user,  load average: 2.50, 2.46, 2.40 > unitime > 773acdb141003855164c5440b40d443a9cc3ff04

--- include/config.h
+++ include/config.h
@@ -3,8 +3,8 @@
 
 // #define NEMU_REF_INSTR
 
-// PA 
-#define CACHE_ENABLED
+// PA 3
+//#define CACHE_ENABLED
 //#define IA32_SEG             // protect mode enable
 //#define IA32_PAGE            // virtual memory management is now complete
 //#define TLB_ENABLED
