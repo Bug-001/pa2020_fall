c3a7559b2d57cd703cbb95adff1439342726483b
tracer-pa-public <tracer@njuics.org>
2020-12-08 23:12:08 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:12:08 up 92 days, 14:26,  1 user,  load average: 2.39, 2.37, 2.40 > unitime 1607440328556 > bac161205cc06546d2b066ab44e7f58d1f65ca22

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -55,10 +55,13 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data)
 // read data from cache
 uint32_t cache_read(paddr_t paddr, size_t len)
 {
-    printf("cache: implement me in PA 3-1\n");
-    fflush(stdout);
-    assert(0);
-	// implement me in PA 3-1
+    int row = get_cache_set_index(paddr);
+    int tag = get_tag(paddr);
+    LineSet ls = cache[row];
+    for(int i = 0; i < 8; ++i)
+    {
+        if(ls[i].valid_bit == 1 && ls[i].tag == tag);
+    }
 	return 0;
 }
 
