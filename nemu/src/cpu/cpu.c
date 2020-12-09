6eedfd6d7df67cacbef1cddb6c31d37fc671ecf2
tracer-pa-public <tracer@njuics.org>
2020-12-09 14:04:18 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 14:04:18 up 93 days,  5:18,  1 user,  load average: 2.60, 2.68, 2.65 > unitime 1607493858211 > ed3c096ea1aa15d9a6aeef2e9dd253f934275a33

--- nemu/src/cpu/cpu.c
+++ nemu/src/cpu/cpu.c
@@ -111,7 +111,7 @@ void exec(uint32_t n)
 	if (nemu_state == NEMU_STOP)
 	{
 #ifdef CACHE_ENABLED
-        printf("hw_mem_access_time_cache = %lld, hw_mem_access_time_no_cache = %lld, \e[0;32mSpeedup: %f\e[0m, hit_rate = %f\n", hw_mem_access_time_cache, hw_mem_access_time_no_cache, (double)hw_mem_access_time_no_cache / (double)hw_mem_access_time_cache, (double)hit_in_cache / (double)total_visit);
+        printf("hw_mem_access_time_cache = %lld, hw_mem_access_time_no_cache = %lld, \e[0;32mSpeedup: %f\e[0m, \e[0;32mhit_rate = %f\%\e[0m\n", hw_mem_access_time_cache, hw_mem_access_time_no_cache, (double)hw_mem_access_time_no_cache / (double)hw_mem_access_time_cache, (double)hit_in_cache / (double)total_visit * 100);
         fflush(stdout);
 #endif
 		printf("NEMU2 terminated\n");
