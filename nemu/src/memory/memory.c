8db067a8c14db133b0a8d2a79ce18a880fe351fb
tracer-pa-public <tracer@njuics.org>
2020-12-09 13:11:01 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 13:11:01 up 93 days,  4:25,  1 user,  load average: 2.53, 2.59, 2.61 > unitime 1607490661669 > 838132ff635fb80e22d0381bfb762930128f9fe1

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -28,7 +28,7 @@ uint32_t paddr_read(paddr_t paddr, size_t len)
 		temp = hw_mem_read(paddr, len);
 		if(ret != temp)
 		{
-		    printf("ret = %u, temp = %u\n", ret, temp);
+		    printf("ret = %x, temp = %x\n", ret, temp);
 		    fflush(stdout);
 		    assert(0);
 		}
