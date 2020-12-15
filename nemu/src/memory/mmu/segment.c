235c4710fc1fa7dc32f0989fa8b1ec5a65adfe96
tracer-pa-public <tracer@njuics.org>
2020-12-15 19:41:33 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:41:33 up 99 days, 10:56,  2 users,  load average: 2.17, 2.42, 2.53 > unitime 1608032493684 > 682c26a06d6acbe0197c97ec40797c3b6d58f12f

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -4,9 +4,7 @@
 // return the linear address from the virtual address and segment selector
 uint32_t segment_translate(uint32_t offset, uint8_t sreg)
 {
-	/* TODO: perform segment translation from virtual address to linear address
-	 * by reading the invisible part of the segment register 'sreg'
-	 */
+	// return cpu.segReg[sreg].base + offset;
 	return 0;
 }
 
