ace82091c1c56f2bdcb4b23800379844128a49fd
tracer-pa-public <tracer@njuics.org>
2020-12-20 16:56:07 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:56:07 up 104 days,  8:10,  1 user,  load average: 1.79, 1.66, 1.47 > unitime 1608454567734 > f339659d8c13a357b4c58cd7b3e15bc51819edb2

--- nemu/src/cpu/cpu.c
+++ nemu/src/cpu/cpu.c
@@ -48,6 +48,7 @@ void init_cpu(const uint32_t init_eip)
 void exec(uint32_t n)
 {
 	static BP *bp = NULL;
+	static int count = 2000;
 	verbose = (n <= 100000);
 	int instr_len = 0;
 	bool hit_break_rerun = false;
@@ -65,7 +66,11 @@ void exec(uint32_t n)
 			instr_len = exec_inst();
 			cpu.eip += instr_len;
 			n--;
-			printf("eip = 0x%08x\n", cpu.eip);
+			if(count)
+			{
+			    printf("eip = 0x%08x\n", cpu.eip);
+			    --count;
+			}
 
 			if (hit_break_rerun)
 			{
