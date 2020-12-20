a06d02543d0a5d0ff61ba6c7e508d276b366565e
tracer-pa-public <tracer@njuics.org>
2020-12-20 12:21:06 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 12:21:06 up 104 days,  3:35,  1 user,  load average: 1.38, 1.31, 1.62 > unitime 1608438066547 > 5cef63b1f53bc9792224bf98204f3d4f462333a1

--- nemu/include/cpu/instr/lgdt.h
+++ nemu/include/cpu/instr/lgdt.h
@@ -1,7 +1,6 @@
 #ifndef __INSTR_LGDT_H__
 #define __INSTR_LGDT_H__
-/*
-Put the declarations of `lgdt' instructions here.
-*/
+
+make_instr_func(lgdt);
 
 #endif
--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -1,4 +1,6 @@
 #include "cpu/instr.h"
-/*
-Put the implementations of `lgdt' instructions here.
-*/
+
+make_instr_func(lgdt)
+{
+    return 0;
+}
