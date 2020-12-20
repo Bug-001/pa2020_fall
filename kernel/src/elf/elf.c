85db33a5651afb83a091f78d6b146728dbf89547
tracer-pa-public <tracer@njuics.org>
2020-12-20 18:28:28 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:28:28 up 104 days,  9:42,  1 user,  load average: 1.38, 1.34, 1.33 > unitime 1608460108140 > eec7fe995df26b615d14e3083892a1196b92caa

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -57,7 +57,10 @@ uint32_t loader()
 	volatile uint32_t entry = elf->e_entry;
 
 #ifdef IA32_PAGE
-	mm_malloc(KOFFSET - STACK_SIZE, STACK_SIZE);
+	uint32_t temp = mm_malloc(KOFFSET - STACK_SIZE, STACK_SIZE);
+	char s[20];
+	sprintf(s, "%08x", temp);
+	Log(s);
 #ifdef HAS_DEVICE_VGA
 	create_video_mapping();
 #endif
