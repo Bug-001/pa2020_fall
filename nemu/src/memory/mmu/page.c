b2041f5504fe58364db93256d72a9cdd78d8b436
tracer-pa-public <tracer@njuics.org>
2020-12-20 21:13:31 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:13:31 up 104 days, 12:27,  1 user,  load average: 1.26, 1.12, 1.19 > unitime 1608470011481 > 8b13c42a94cf7aaa2c8fcbaf52567ee14cbf873c

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -4,6 +4,7 @@
 // translate from linear address to physical address
 paddr_t page_translate(laddr_t laddr)
 {
+#ifdef IA32_PAGE
 #ifndef TLB_ENABLED
 	uint32_t dir = laddr >> 22; // 高10位
 	uint32_t page = (laddr & 0x3FFFFF) >> 12; // 中间10位
@@ -16,5 +17,6 @@ paddr_t page_translate(laddr_t laddr)
 	return (page_table->page_frame << 12) + offset;
 #else
 	return tlb_read(laddr) | (laddr & PAGE_MASK);
+#endif
 #endif
 }
