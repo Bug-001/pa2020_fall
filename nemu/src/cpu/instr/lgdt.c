243c44c077a0c0ddd38b1d65543aec24a3f18f2d
tracer-pa-public <tracer@njuics.org>
2020-12-20 13:55:21 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 13:55:21 up 104 days,  5:09,  1 user,  load average: 1.01, 1.25, 1.34 > unitime 1608443721208 > ae65faca11dd247877e137fed44508a2dea56d

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -2,5 +2,9 @@
 
 make_instr_func(lgdt)
 {
-    return 0;
+    int base = paddr_read(eip + 1, 4);
+    int limit = paddr_read(eip + 5, 2);
+    cpu.gdtr.base = base;
+    cpu.gdtr.limit = limit;
+    return 7;
 }
