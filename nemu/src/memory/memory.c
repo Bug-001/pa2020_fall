6bc062ee2d24ec9f100f725a994297ab29479ec6
tracer-pa-public <tracer@njuics.org>
2020-12-20 12:10:31 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 12:10:31 up 104 days,  3:25,  1 user,  load average: 1.50, 1.67, 2.06 > unitime 1608437431807 > 73e61458bf7fc13dc3465b0713b5784ef3bceb72

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -69,7 +69,14 @@ uint32_t vaddr_read(vaddr_t vaddr, uint8_t sreg, size_t len)
 void vaddr_write(vaddr_t vaddr, uint8_t sreg, size_t len, uint32_t data)
 {
 	assert(len == 1 || len == 2 || len == 4);
+#ifndef IA32_SEG
 	laddr_write(vaddr, len, data);
+#else
+	uint32_t laddr = vaddr;
+	if( cpu.cr0.pe == 1 ) {
+		laddr = segment_translate(vaddr, sreg);
+	}
+	return laddr_write(laddr, len, data);
 }
 
 void init_mem()
