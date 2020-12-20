6e11317a5ef95a7d62e9b174b49ab53c74aaf27d
tracer-pa-public <tracer@njuics.org>
2020-12-20 18:00:43 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:00:43 up 104 days,  9:15,  1 user,  load average: 0.90, 1.14, 1.28 > unitime 1608458443013 > b778628cd499f19b59dd37c72247a7b73fc8605e

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -5,9 +5,15 @@
 paddr_t page_translate(laddr_t laddr)
 {
 #ifndef TLB_ENABLED
-	printf("\nPlease implement page_translate()\n");
-	fflush(stdout);
-	assert(0);
+	uint32_t dir = laddr >> 22; // 高10位
+	uint32_t page = (laddr & 0x3FFFFF) >> 12; // 中间10位
+	uint32_t offset = laddr & 0xFFF;
+	PDE* page_dir = (void*)(get_hw_mem() + cpu.cr3.base);
+	page_dir += dir;
+	assert(page_dir->present == 1);
+	PTE* page_table = (void*)(get_hw_mem() + page_dir->page_frame);
+	page_table += page;
+	return (page_table->page_frame << 12) + offset;
 #else
 	return tlb_read(laddr) | (laddr & PAGE_MASK);
 #endif
