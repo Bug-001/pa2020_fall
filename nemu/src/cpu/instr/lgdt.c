b26842a673f5daeedd7d1288e750a1d66939ab6c
tracer-pa-public <tracer@njuics.org>
2020-12-20 21:33:06 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:33:06 up 104 days, 12:47,  1 user,  load average: 1.75, 1.38, 1.25 > unitime 1608471186755 > abdc70f32769d0fc82741863659a49d3e0b44253

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -1,20 +1,12 @@
 #include "cpu/instr.h"
 
-static void instr_execute_1op()
-{
-    printf("opr_src.addr = %08x\n", opr_src.addr);
-    cpu.gdtr.limit = paddr_read(opr_src.addr, 2);
-    cpu.gdtr.base = paddr_read(opr_src.addr + 2, 4);
-    printf("limit = %08x\n", cpu.gdtr.limit);
-    printf("base = %08x\n", cpu.gdtr.base);
-}
-
 make_instr_func(lgdt)                                                                 
 {                                                                                                                           
 	int len = 1;                                                                                                            
 	opr_src.data_size = data_size;                                                                                     
 	len += modrm_rm(eip + 1, &opr_src);
 	print_asm_1("lgdt", "", len + 1, &opr_src); 
-	instr_execute_1op();                                                                                                    
-	return len;                                                                                                             
+	cpu.gdtr.limit = paddr_read(opr_src.addr, 2);
+    cpu.gdtr.base = paddr_read(opr_src.addr + 2, 4);
+    return len;                                                                                                             
 }
