1f9da1bbbbec2b677001d69676eb3b0eafa05f19
tracer-pa-public <tracer@njuics.org>
2020-12-20 18:06:40 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:06:40 up 104 days,  9:21,  1 user,  load average: 1.42, 1.33, 1.31 > unitime 1608458800004 > 14bbb82b1a949de5fcb801a037451366728f2c16

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -1,6 +1,8 @@
 #include "cpu/cpu.h"
 #include "memory/memory.h"
 
+uint8_t 
+
 // translate from linear address to physical address
 paddr_t page_translate(laddr_t laddr)
 {
@@ -8,10 +10,10 @@ paddr_t page_translate(laddr_t laddr)
 	uint32_t dir = laddr >> 22; // 高10位
 	uint32_t page = (laddr & 0x3FFFFF) >> 12; // 中间10位
 	uint32_t offset = laddr & 0xFFF;
-	PDE* page_dir = (void*)(get_hw_mem() + cpu.cr3.base);
+	PDE* page_dir = (void*)(get_mem_addr() + cpu.cr3.base);
 	page_dir += dir;
 	assert(page_dir->present == 1);
-	PTE* page_table = (void*)(get_hw_mem() + page_dir->page_frame);
+	PTE* page_table = (void*)(get_mem_addr() + (page_dir->page_frame << 12));
 	page_table += page;
 	return (page_table->page_frame << 12) + offset;
 #else
