9ac90b1800213a7434713380d529f5c69b5f4a13
tracer-pa-public <tracer@njuics.org>
2020-12-22 01:02:41 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 01:02:41 up 105 days, 16:17,  2 users,  load average: 1.10, 1.20, 1.25 > unitime 1608570161427 > 644c8a2f4526e1b2a400d72245a10c26d6aca0da

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -7,29 +7,29 @@ paddr_t page_translate(laddr_t laddr)
 {
 
 #ifndef TLB_ENABLED
-    printf("laddr = 0x%08x, Page Translate!\n", laddr);
+if(verbose){
+     printf("laddr = 0x%08x, Page Translate!\n", laddr);
     fflush(stdout);
+}
+   
     
 	uint32_t dir = laddr >> 22; // 高10位
 	uint32_t page = (laddr & 0x3FFFFF) >> 12; // 中间10位
 	uint32_t offset = laddr & 0xFFF;
 	
-	printf("cpu.cr3.base = 0x%05x\n", cpu.cr3.base);
-	fflush(stdout);
-	
 	PDE* page_dir = (void*)(get_mem_addr() + (cpu.cr3.base << 12));
 	page_dir += dir;
-	
-	printf("page_dir = 0x%08x\n", page_dir->val);
-	fflush(stdout);
 	
 	assert(page_dir->present == 1);
 	PTE* page_table = (void*)(get_mem_addr() + (page_dir->page_frame << 12));
 	page_table += page;
 	assert(page_table->present == 1);
 	
-	printf("Page Translate OK, paddr = 0x%08x\n", (page_table->page_frame << 12) + offset);
+	if(verbose){
+	    	printf("Page Translate OK, paddr = 0x%08x\n", (page_table->page_frame << 12) + offset);
 	fflush(stdout);
+	}
+
 	
 	return (page_table->page_frame << 12) + offset;
 #else
