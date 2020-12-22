d815f5aa454c28391ac298806550ed6919111a0e
tracer-pa-public <tracer@njuics.org>
2020-12-22 17:52:03 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:52:03 up 106 days,  9:06,  1 user,  load average: 1.35, 1.39, 1.33 > unitime 1608630723563 > dc43401db48971c67ac7f39379ab8e3145cba3d5

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -3,13 +3,14 @@
 
 make_instr_func(lidt)
 {
-#ifdef IA32_INTR
 	int len = 1;
 	opr_src.data_size = data_size;
 	len += modrm_rm(eip + 1, &opr_src);
 	print_asm_1("lidt", "", len + 1, &opr_src); 
+#ifdef IA32_INTR
 	cpu.idtr.limit = paddr_read(opr_src.addr, 2);
     cpu.idtr.base = paddr_read(opr_src.addr + 2, 4);
-    return len;
 #endif
+    return len;
+
 }
