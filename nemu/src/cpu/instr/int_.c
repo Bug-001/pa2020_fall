61d7067dfd3c9e793b6b90af38c67975bfa55c80
tracer-pa-public <tracer@njuics.org>
2021-01-08 19:44:26 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:44:26 up 123 days, 10:58,  2 users,  load average: 1.59, 1.38, 1.37 > unitime 1610106266705 > e8352f1dc9ad30ac8debdef84850a0616e3fcac9

--- nemu/src/cpu/instr/int_.c
+++ nemu/src/cpu/instr/int_.c
@@ -10,13 +10,14 @@ Special note for `int': please use the instruction name `int_' instead of `int'.
 make_instr_func(int_)
 {
     assert(cpu.cr0.pe == 1);
-    uint8_t intr_no = instr_fetch(eip + 1, 1);
-    raise_sw_intr(intr_no);
     
     OPERAND temp;
-    temp.val = intr_no;
     temp.type = OPR_IMM;
-    
+    temp.addr = eip + 1;
+    temp.size = 8;
+    temp.sreg = SREG_CS;
+    operand_read(&temp);
+    raise_sw_intr(temp.val);
     print_asm_1("int", "", 2, &temp);
     
     return 0;
