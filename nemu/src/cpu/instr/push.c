b5454fa29fd9e352147a71c010eae56c9cc85b49
tracer-pa-public <tracer@njuics.org>
2021-01-08 20:41:12 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:41:12 up 123 days, 11:55,  2 users,  load average: 1.77, 1.39, 1.55 > unitime 1610109672290 > 81f2382f4498fbaf70b59953cee4370ce7690907

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -2,7 +2,7 @@
 
 static void instr_execute_1op()
 {
-    cpu.esp -= data_size / 8;
+    cpu.esp -= opr_src.data_size / 8;
     
     operand_read(&opr_src);
     
@@ -10,7 +10,7 @@ static void instr_execute_1op()
     s.type = OPR_MEM;
     s.addr = cpu.esp;
     s.val = opr_src.val;
-    s.data_size = data_size;
+    s.data_size = opr_src.data_size;
     s.sreg = SREG_SS;
     
     operand_write(&s);
