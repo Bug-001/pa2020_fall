1d9288220b1def941662b7aef763a96572a2f8c7
tracer-pa-public <tracer@njuics.org>
2021-01-08 21:24:59 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:24:59 up 123 days, 12:39,  2 users,  load average: 0.98, 1.49, 1.50 > unitime 1610112299651 > 847b03e893b1158f71f484c68889788adc1aa805

--- include/config.h
+++ include/config.h
@@ -7,11 +7,11 @@
 #define CACHE_ENABLED
 #define IA32_SEG             // protect mode enable
 #define IA32_PAGE            // virtual memory management is now complete
-//#define TLB_ENABLED
+#define TLB_ENABLED
 
 // PA 4
-//#define IA32_INTR            // tells NEMU and Kernel that we are ready for handling exceptions(traps only) and interrupts
-//#define HAS_DEVICE_TIMER
+#define IA32_INTR            // tells NEMU and Kernel that we are ready for handling exceptions(traps only) and interrupts
+#define HAS_DEVICE_TIMER
 //#define HAS_DEVICE_SERIAL    // the disturbing 'nemu trap output:' tag will be removed
 //#define HAS_DEVICE_IDE       // the loader will load the elf from the hard disk instead of mem disk
 //#define HAS_DEVICE_KEYBOARD  // keyboard input
