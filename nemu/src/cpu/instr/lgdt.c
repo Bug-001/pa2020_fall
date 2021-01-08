a6f04a1b7bbe3ca0b77b9dd7fcbb81b958e1805d
tracer-pa-public <tracer@njuics.org>
2021-01-08 19:28:48 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:28:48 up 123 days, 10:43,  2 users,  load average: 1.23, 1.37, 1.35 > unitime 1610105328453 > aac8fe5d722495a0939ab60a1a19daa9218ad546

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -6,7 +6,7 @@ make_instr_func(lgdt)
 	opr_src.data_size = data_size;
 	len += modrm_rm(eip + 1, &opr_src);
 	print_asm_1("lgdt", "", len + 1, &opr_src);
-	if(cpu.cr0.pe == 1)
+	if(cpu.cr0.pg == 1)
 	{
 	    cpu.gdtr.limit = laddr_read(opr_src.addr, 2);
         cpu.gdtr.base = laddr_read(opr_src.addr + 2, 4);
