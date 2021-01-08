1079725aa30580f2323871a168e7dd8be38e514f
tracer-pa-public <tracer@njuics.org>
2021-01-08 19:23:46 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:23:46 up 123 days, 10:38,  2 users,  load average: 1.44, 1.39, 1.35 > unitime 1610105025968 > 7b2ac1a35d4578da4ee640ed30cb5069d0e53d24

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -53,6 +53,7 @@ void init()
 void init_cond()
 {
 #ifdef IA32_INTR
+BREAK_POINT;
 	/* Reset the GDT, since the old GDT in start.S cannot be used in the future. */
 	init_segment();
 
@@ -60,7 +61,7 @@ void init_cond()
 	 * Note that system call is the only exception implemented in NEMU.
 	 */
 	init_idt();
-
+    
 	/* Enable interrupts. */
 	sti();
 #endif
