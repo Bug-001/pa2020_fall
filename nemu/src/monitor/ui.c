66c9803db4af6b1889888d346111158c785c1d58
tracer-pa-public <tracer@njuics.org>
2021-01-10 14:08:58 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 14:08:58 up 125 days,  5:23,  2 users,  load average: 1.43, 1.47, 1.44 > unitime 1610258938422 > c83deb077ec789797af2561784fae1c2f09e3fc

--- nemu/src/monitor/ui.c
+++ nemu/src/monitor/ui.c
@@ -309,12 +309,9 @@ cmd_handler(cmd_help)
 	return 0;
 }
 
-extern int set_ref();
-
 // the main loop of accepting user commands
 void ui_mainloop(bool autorun)
 {
-    set_ref();
 	if (autorun)
 	{
 		cmd_c("");
