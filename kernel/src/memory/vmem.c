6d567f3da7050a3cbcaddf4787fe5b438d03c905
tracer-pa-public <tracer@njuics.org>
2021-01-13 12:31:08 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 12:31:08 up 128 days,  3:45,  1 user,  load average: 1.23, 1.29, 1.42 > unitime 1610512268530 > 7c520e454c3f5c53495265efde905c6bdd8fa9c2

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -15,7 +15,7 @@ void create_video_mapping()
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
-    PDE* p = get_updir();
+    // PDE* p = get_updir();
     
 }
 
