c59c2006eeef0d7bc417e5c8aa0735f4a3488527
tracer-pa-public <tracer@njuics.org>
2021-01-13 11:25:39 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:25:39 up 128 days,  2:40,  1 user,  load average: 1.32, 1.23, 1.26 > unitime 1610508339559 > 1aed9ad18b07f6908520bedf701d946780fe77b

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -1,5 +1,31 @@
 #include "cpu/instr.h"
 #include "device/port_io.h"
-/*
-Put the implementations of `in' instructions here.
-*/
+
+static int in(int size)
+{
+    OPERAND DX, A;
+    DX.type = OPR_REG;
+    DX.addr = REG_DX;
+    DX.data_size = 16;
+    A.type = OPR_REG;
+    A.addr = REG_EAX;
+    A.data_size = size;
+    
+    print_asm_2("in", "", &DX, &A);
+    
+    operand_read(&DX);
+    A.val = pio_read(DX.val, size);
+    operand_write(&A);
+    
+    return 1;
+}
+
+make_instr_func(in_b)
+{
+    return in(8);
+}
+
+make_instr_func(in_v)
+{
+    return in(data_size);
+}
--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -1,5 +1,31 @@
 #include "cpu/instr.h"
 #include "device/port_io.h"
-/*
-Put the implementations of `out' instructions here.
-*/
+
+static int out(int size)
+{
+    OPERAND DX, A;
+    DX.type = OPR_REG;
+    DX.addr = REG_DX;
+    DX.data_size = 16;
+    A.type = OPR_REG;
+    A.addr = REG_EAX;
+    A.data_size = size;
+    
+    print_asm_2("out", "", &DX, &A);
+    
+    operand_read(&A);
+    operand_read(&DX);
+    pio_write(DX.val, size, A.val);
+    
+    return 1;
+}
+
+make_instr_func(out_b)
+{
+    return out(8);
+}
+
+make_instr_func(out_v)
+{
+    return out(data_size);
+}
