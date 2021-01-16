7731421629a392e0243914960d85f589b15e2dca
tracer-pa-public <tracer@njuics.org>
2021-01-16 23:56:30 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:56:30 up 131 days, 15:10,  1 user,  load average: 2.24, 1.71, 1.63 > unitime 1610812590421 > 1d482b30809d67443dabaabfb95503a0f199d211

--- include/config.h
+++ include/config.h
@@ -7,7 +7,7 @@
 #define CACHE_ENABLED
 #define IA32_SEG             // protect mode enable
 #define IA32_PAGE            // virtual memory management is now complete
-//#define TLB_ENABLED
+#define TLB_ENABLED
 
 // PA 4
 #define IA32_INTR            // tells NEMU and Kernel that we are ready for handling exceptions(traps only) and interrupts
@@ -16,7 +16,7 @@
 #define HAS_DEVICE_IDE       // the loader will load the elf from the hard disk instead of mem disk
 #define HAS_DEVICE_KEYBOARD  // keyboard input
 #define HAS_DEVICE_VGA       // we then have display
-//#define HAS_DEVICE_AUDIO     // audio, this is experimental, need to enable when submit pa-4-3
+#define HAS_DEVICE_AUDIO     // audio, this is experimental, need to enable when submit pa-4-3
 
 #define PA_ONLINE
 
