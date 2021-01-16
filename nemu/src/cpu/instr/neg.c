77e0c8021bf5cdd6bb95558d1a628052b3e8d768
tracer-pa-public <tracer@njuics.org>
2021-01-17 00:20:59 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 00:20:59 up 131 days, 15:35,  1 user,  load average: 1.40, 1.59, 1.68 > unitime 1610814059622 > 1d1ce359af566e7137f64549525a86e3ef3b1e46

--- nemu/src/cpu/instr/neg.c
+++ nemu/src/cpu/instr/neg.c
@@ -1,4 +1,11 @@
 #include "cpu/instr.h"
-/*
-Put the implementations of `neg' instructions here.
-*/
+
+static int instr_execute_1op()
+{
+    operand_read(&opr_src);
+    opr_src.val = alu_mul(opr_src.val, 0xFFFFFFFF, data_size);
+    operand_write(&opr_src);
+}
+
+make_instr_impl_1op(neg, rm, b)
+make_instr_impl_1op(neg, rm, v)
