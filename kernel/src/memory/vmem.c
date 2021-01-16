8e2012d4da34b09552cda900a72c6c6fb3ece79e
tracer-pa-public <tracer@njuics.org>
2021-01-16 22:25:43 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:25:43 up 131 days, 13:40,  1 user,  load average: 1.35, 1.62, 1.69 > unitime 1610807143647 > 90b939a590ace733ec4a702b5104723d3cc8e38e

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -19,13 +19,13 @@ void create_video_mapping()
     PDE* pdir = get_updir();
     memcpy(vmem_table, (void *)(pdir[VMEM_ADDR / PT_SIZE].page_frame << 12), sizeof(vmem_table));
     // assert(pdir[VMEM_ADDR / PT_SIZE].present == 1);
-    pdir[VMEM_ADDR / PT_SIZE].val = make_pde(vmem_table);
+    pdir[VMEM_ADDR / PT_SIZE].val = make_pde(va_to_pa(vmem_table));
     // PTE* ptable = (PTE *)(pdir[VMEM_ADDR / PT_SIZE].page_frame << 12);
     uint32_t pframe_idx = VMEM_ADDR >> 12;
     PTE* ptable = vmem_table + (pframe_idx & 0x3ff);
     for(int cnt = 0; cnt < NR_PT; ++cnt)
     {
-        ptable->val = make_pte(pframe_idx << 12);
+        ptable->val = make_pte(va_to_pa(pframe_idx << 12));
         ++pframe_idx;
         ++ptable;
     }
