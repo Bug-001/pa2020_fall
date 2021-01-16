0c2de045f6d30cbec1acbc2f03e5e4c05fd54392
tracer-pa-public <tracer@njuics.org>
2021-01-17 00:21:20 +0800

    > compile nemu testcase kernel > id 191850169 > user pa191850169 > uname Linux 0d39dc9d5e30 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 00:21:20 up 131 days, 15:35,  1 user,  load average: 1.62, 1.64, 1.69 > unitime 1610814080071 > 62d7d802bde7457b4c337c21ad8b8b501cd997ae

--- nemu/src/cpu/instr/neg.c
+++ nemu/src/cpu/instr/neg.c
@@ -1,6 +1,6 @@
 #include "cpu/instr.h"
 
-static int instr_execute_1op()
+static void instr_execute_1op()
 {
     operand_read(&opr_src);
     opr_src.val = alu_mul(opr_src.val, 0xFFFFFFFF, data_size);
