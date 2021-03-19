7b3898d9ca28108e6eb094e4933ed6e3ab71a22d
tracer-pa-public <tracer@njuics.org>
2021-03-19 11:00:22 +0800

    > compile nemu testcase kernel > id 191850169 > user suhaitao > uname Linux SuHaitao-HP 5.8.0-45-generic #51~20.04.1-Ubuntu SMP Tue Feb 23 13:46:31 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux > uptime 11:00:22 up  1:07,  3 users,  load average: 0.70, 0.54, 0.78 > unitime > 1d12757a35890f146ae5a7e2538252169df9bf0f

--- test/temp.c
+++
@@ -1,5 +0,0 @@
-int main()
-{
-    int a=3, b=2, c;
-    c=-a/b;
-}
--- test/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	int x = -2147483648;
-	printf("%d\n", x >> 31);
-	unsigned int y = -2147483648;
-	printf("%d\n", y >> 31);
-	return 0;
-}
